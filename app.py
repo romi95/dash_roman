0a8419825c2b0516067910e4d3d7ecb04db08272
romi95 <34400881+romi95@users.noreply.github.com>
2017-12-09 18:00:39 +0100

    1

---
+++ app.py
@@ -0,0 +1,156 @@
+
+# coding: utf-8
+
+# In[63]:
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
+import pandas as pd
+
+df = pd.read_csv(
+    'nama_10_gdp_1_Data.csv')
+
+df
+
+
+# In[72]:
+
+app = dash.Dash(__name__)
+server = app.server
+app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
+available_indicators = df['UNIT'].unique()
+available_geo = df['GEO'].unique()
+
+app.layout = html.Div([
+    html.H1(children='Data visualization project'),
+    html.H2(children='Fernandez\nRoman'),
+    #FIRST GRAPH
+    html.Div([
+        html.H2(children='1st graph',style={'text-align':'left','font-family':'monospace'}),
+        html.Div([
+            html.Div([
+                dcc.Dropdown(
+                    id='xaxis-column',
+                    options=[{'label': i, 'value': i} for i in available_indicators],
+                    value='Current prices, million euro'
+                ),
+            ],
+            style={'width': '48%', 'display': 'inline-block'}),
+
+            html.Div([
+                dcc.Dropdown(
+                    id='yaxis-column',
+                    options=[{'label': i, 'value': i} for i in available_indicators],
+                    value='Chain linked volumes (2010),million euro'
+                ),
+            ],style={'width': '48%', 'float': 'right', 'display': 'inline-block'})
+        ]),
+
+        dcc.Graph(id='indicator-graphic'),
+
+        dcc.Slider(
+            id='year--slider',
+            min=df['TIME'].min(),
+            max=df['TIME'].max(),
+            value=2010,
+            step=None,
+            marks={str(year): str(year) for year in df['TIME'].unique()}
+        )
+    ]),
+    #SECOND GRAPH
+    html.Div([
+        html.H2(children='Second graph',style={'margin-top':'5%','text-align':'center','font-family':'monospace'}),
+        html.Div([
+            html.Div([
+                dcc.Dropdown(
+                    id='indicator-select',
+                    options=[{'label': i, 'value': i} for i in available_indicators],
+                    value='Current prices, million euro'
+                ),
+            ],
+            style={'width': '48%', 'display': 'inline-block'}),
+
+            html.Div([
+                dcc.Dropdown(
+                    id='country-select',
+                    options=[{'label': i, 'value': i} for i in available_geo],
+                    value='Spain'
+                ),
+            ],style={'width': '48%', 'float': 'right', 'display': 'inline-block'})
+        ]),
+        dcc.Graph(id='indicator-graphic2'),
+    ])
+    #END SECOND
+],style={'background-color':'rgb(215, 220, 241)'})
+
+@app.callback(
+    dash.dependencies.Output('indicator-graphic', 'figure'),
+    [dash.dependencies.Input('xaxis-column', 'value'),
+     dash.dependencies.Input('yaxis-column', 'value'),
+     dash.dependencies.Input('year--slider', 'value')])
+def update_graph(xaxis_column_name, yaxis_column_name, year_value):
+    dff = df[df['TIME'] == year_value]
+
+    return {
+        'data': [go.Scatter(
+            x=dff[dff['UNIT'] == xaxis_column_name]['Value'],
+            y=dff[dff['UNIT'] == yaxis_column_name]['Value'],
+            text=dff[dff['UNIT'] == yaxis_column_name]['GEO'],
+            mode='markers',
+            marker={
+                'size': 10,
+                'color':'rgb(22, 96, 167)',
+                'opacity': 0.5,
+                'line': {'width': 0.5, 'color': 'white'}
+            }
+        )],
+        'layout': go.Layout(
+            xaxis={
+                'title': xaxis_column_name,
+                'type': 'linear'
+            },
+            yaxis={
+                'title': yaxis_column_name,
+                'type': 'linear'
+            },
+            margin={'l': 70, 'b': 40, 't': 10, 'r': 0},
+            hovermode='closest'
+        )
+    }
+#CALLBACK SECOND GRAPH
+@app.callback(
+    dash.dependencies.Output('indicator-graphic2', 'figure'),
+    [dash.dependencies.Input('indicator-select', 'value'),
+     dash.dependencies.Input('country-select', 'value'),])
+def update_graph(indicator_name, country_name):
+    dff = df[df['GEO'] == country_name]
+
+    return {
+        'data': [go.Scatter(
+            x=dff[dff['UNIT'] == indicator_name]['TIME'],
+            y=dff[dff['UNIT'] == indicator_name]['Value'],
+            text=dff[dff['UNIT'] == indicator_name]['Value'],
+            mode='lines',
+            line = dict(
+                color = ('rgb(22, 96, 167)'),
+                width = 4,)
+        )],
+        'layout': go.Layout(
+            xaxis={
+                'title': country_name,
+            },
+            yaxis={
+                'title': indicator_name,
+            },
+            margin={'l': 70, 'b': 40, 't': 10, 'r': 0},
+            hovermode='closest'
+        )
+    }
+
+if __name__ == '__main__':
+    app.run_server()
+
+
+# In[ ]:
